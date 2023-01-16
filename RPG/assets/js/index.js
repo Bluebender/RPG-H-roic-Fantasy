db62045c852c84ce4c21e7a8c0cd694984180496
killyan35 <kilyan.gerard@3wa.io>
2023-01-16 11:59:23 +0000

    Class scene test

--- RPG/assets/js/index.js
+++ RPG/assets/js/index.js
@@ -1,12 +1,12 @@
-import { Scene } from './classes/form.js'
-import { SceneTest } from './classes/form.js'
+import { Scene } from './class/scene.js'
+import { SceneTest } from './class/scenetest.js'
 
 
 window.addEventListener("DOMContentLoaded", function(){
 
-    let scene1 = new Scene("Vous êtes quelque part", "./img/door.png", "Voulez vous faire ça ?", "Oui", "Non");
-    let scene2 = new Scene("Vous êtes autre part", "./img/portal.png", "Voulez vous faire ça ?", "Oui", "Non");
-    let scene3 = new Scene("Vous êtes mort", "./img/tower.png", "Pas de bol");
+    let scene1 = new Scene("Vous êtes quelque part", "./img/door.png", "Voulez vous faire ça ?", "Oui", "Non", "null", "null");
+    let scene2 = new Scene("Vous êtes autre part", "./img/portal.png", "Voulez vous faire ça ?", "Oui", "Non", "null", "null");
+    let scene3 = new Scene("Vous êtes mort", "./img/tower.png", "Pas de bol", "null", "null", "null", "null");
 
 
     
