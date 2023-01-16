36207e4a07962feede4de2495f4338cbc02e54cb
Bluebender <olliviev@gmail.com>
2023-01-16 11:47:17 +0000

    Sauvegarde de 12h47

---
+++ RPG/assets/js/index.js
@@ -0,0 +1,13 @@
+import { Scene } from './classes/form.js'
+import { SceneTest } from './classes/form.js'
+
+
+window.addEventListener("DOMContentLoaded", function(){
+
+    let scene1 = new Scene("Vous êtes quelque part", "./img/door.png", "Voulez vous faire ça ?", "Oui", "Non");
+    let scene2 = new Scene("Vous êtes autre part", "./img/portal.png", "Voulez vous faire ça ?", "Oui", "Non");
+    let scene3 = new Scene("Vous êtes mort", "./img/tower.png", "Pas de bol");
+
+
+    
+});
