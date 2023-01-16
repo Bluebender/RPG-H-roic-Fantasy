9da49d92382f02d535b93ad42573c62d6b4e3967
killyan35 <kilyan.gerard@3wa.io>
2023-01-16 15:43:38 +0000

    mise a jour game.js

--- RPG/assets/js/class/game.js
+++ RPG/assets/js/class/game.js
@@ -1,3 +1,8 @@
+import { Scene } from './class/scene.js';
+import { Game } from './class/game.js';
+import { SceneTest } from './class/scenetest.js';
+import { GameTest } from './class/gametest.js';
+
 class Game {
     #scenes;
     
@@ -5,16 +10,6 @@ class Game {
         this.#scenes = [];
     }
     
-<<<<<<< HEAD
-    get scenes (){
-        return this.#scenes;
-    }
-    
-    set scenes (scenes){
-        this.#scenes = scenes; 
-    }
-    
-=======
     get scenes () {
         return this.#scenes;
     }
@@ -22,7 +17,6 @@ class Game {
     set scenes(scenes) {
         this.#scenes = scenes;
     }
->>>>>>> 794a9b8bbeff51d588e07be45c61c0872038c016
     ActuallyScene(scene){
         this.#scenes.push(scene);
     }
@@ -31,7 +25,7 @@ class Game {
         
         for(let i = 0; i < this.#scenes.length; i++){
             
-            let intro = document.getElementById('titreIntro');
+            let intro = document.getElementById('titreintro');
             let description = document.getElementById('description');
             let question = document.getElementById('question');
             let choice1 = document.getElementById('choice1');
@@ -40,23 +34,51 @@ class Game {
             let choice4 = document.getElementById('choice4');
             let img = document.getElementById('img');
             
-            let introText = document.createTextNode(this.#scenes[i]);
-            let descriptionText = document.createTextNode(this.#scenes[i]);
-            let questionText = document.createTextNode(this.#scenes[i]);
-            let choice1Text = document.createTextNode(this.#scenes[i]);
-            let choice2Text = document.createTextNode(this.#scenes[i]);
-            let choice3Text = document.createTextNode(this.#scenes[i]);
-            let choice4Text = document.createTextNode(this.#scenes[i]);
+            let introText = document.createTextNode(this.#scenes[i].titreIntro);
+            let descriptionText = document.createTextNode(this.#scenes[i].description);
+            let illustrationimg = document.createTextNode(this.#scenes[i].illustration);
+            let illustrationText = document.createTextNode(this.#scenes[i].imgalt);
+            let questionText = document.createTextNode(this.#scenes[i].question);
+            let choice1Text = document.createTextNode(this.#scenes[i].choice1);
+            let choice2Text = document.createTextNode(this.#scenes[i].choice2);
+            let choice3Text = document.createTextNode(this.#scenes[i].choice3);
+            let choice4Text = document.createTextNode(this.#scenes[i].choice4);
             
             intro.appendChild(introText);
-            img.setAttribute("src", this.#scenes[i].file);
-            img.setAttribute("alt", this.#scenes[i].name);
+            img.setAttribute("src", illustrationimg);
+            img.setAttribute("alt", illustrationText);
             description.appendChild(descriptionText);
             question.appendChild(questionText);
             choice1.appendChild(choice1Text);
-            choice2.appendChild(choice2Text);
-            choice3.appendChild(choice3Text);
-            choice4.appendChild(choice4Text);
+            
+            if (this.#scenes[i].choice4 === "null")
+            {
+                choice4.classList.add("null");
+            }
+            else
+            {
+                choice4.classList.remove("null");
+                choice4.appendChild(choice4Text);
+            }
+            
+            if (this.#scenes[i].choice3 === "null")
+            {
+                choice3.classList.add("null");
+            }
+            else
+            {
+                choice4.classList.remove("null");
+                choice3.appendChild(choice3Text);
+            }
+            if (this.#scenes[i].choice2 === "null")
+            {
+                choice2.classList.add("null");
+            }
+            else
+            {
+                choice4.classList.remove("null");
+                choice2.appendChild(choice2Text);
+            }
         }
         
     }
@@ -64,23 +86,23 @@ class Game {
     
     // Sauvegarde de l'endroit où l'on est
     
-    toJSON(){
+    // toJSON(){
         
-        for(let i = 0; i < this.#scenes.length; i++){
-            let save = {
-                let intro = this.#scenes[i];
-                let description = this.#scenes[i];
-                let question = this.#scenes[i];
-                let choice1 = this.#scenes[i];
-                let choice2 = this.#scenes[i];
-                let choice3 = this.#scenes[i];
-                let choice4 = this.#scenes[i];
-                let img = this.#scenes[i];
-            }
+    //     for(let i = 0; i < this.#scenes.length; i++){
+    //         let save = {
+    //             let intro = this.#scenes[i];
+    //             let description = this.#scenes[i];
+    //             let question = this.#scenes[i];
+    //             let choice1 = this.#scenes[i];
+    //             let choice2 = this.#scenes[i];
+    //             let choice3 = this.#scenes[i];
+    //             let choice4 = this.#scenes[i];
+    //             let img = this.#scenes[i];
+    //         }
             
-            return JSON.stringify(save);
-        }
+    //         return JSON.stringify(save);
+    //     }
         
-    }
+    // }
 }
 export { Game };
--- RPG/assets/js/index.js
+++ RPG/assets/js/index.js
@@ -5,15 +5,11 @@ import { GameTest } from './class/gametest.js'
 
 
 window.addEventListener("DOMContentLoaded", function(){
-
+    let scene = new Game ();
     let scene1 = new Scene("Vous êtes quelque part", "./img/door.png", "Voulez vous faire ça ?", "Oui", "Non", "null", "null");
     let scene2 = new Scene("Vous êtes autre part", "./img/portal.png", "Voulez vous faire ça ?", "Oui", "Non", "null", "null");
     let scene3 = new Scene("Vous êtes mort", "./img/tower.png", "Pas de bol", "null", "null", "null", "null");
-    
-    let test = new SceneTest();
-    test.test();
-
-   let gametest = new GameTest();
-    gametest.test2();
+    scene = scene1;
+    console.log(scene);
     
 });
