6fbd8a323f6452dfcf09a74aa2a28fe2f442f8d1
Bluebender <olliviev@gmail.com>
2023-01-17 14:02:54 +0000

    Sauvegarde de 15h

--- RPG/assets/js/class/game.js
+++ RPG/assets/js/class/game.js
@@ -85,12 +85,47 @@ class Game {
     
     Choose(){
         let choice = document.querySelectorAll("input[name='choice']");
+        let choiceCheked;
 
         for(let i = 0; i < choice.length; i++){
+
             if(choice[i].checked === true){
-                let choiceCheked = choice[i].getAttribute("attr");
+                choiceCheked = choice[i].getAttribute("attr");
                 console.log(choiceCheked)
-                return choiceCheked;
+                
+                let description = document.getElementById('description');
+                let question = document.getElementById('question');
+                let choice1 = document.getElementById('choice1');
+                let choice2 = document.getElementById('choice2');
+                let choice1Input = document.getElementById('choix1');
+                let choice2Input = document.getElementById('choix2');
+                let img = document.getElementById('img');
+                
+                let sceneToDisplay = new Scene(choiceCheked);
+                
+                let descriptionText = document.createTextNode(sceneToDisplay.description);
+                let illustrationimg = sceneToDisplay.illustration;
+                let illustrationText = sceneToDisplay.imgalt;
+                let questionText = document.createTextNode(sceneToDisplay.question);
+                let choice1Text = document.createTextNode(sceneToDisplay.choice1);
+                let choice1Redirect = sceneToDisplay.redirection1;
+                let choice2Text = document.createTextNode(sceneToDisplay.choice2);
+                let choice2Redirect = sceneToDisplay.redirection2;
+        
+                img.setAttribute("src", illustrationimg);
+                img.setAttribute("alt", illustrationText);
+                choice1Input.setAttribute("attr", choice1Redirect);
+                choice2Input.setAttribute("attr", choice2Redirect);
+                
+                description.innerHTML="";
+                question.innerHTML="";
+                choice1.innerHTML="";
+                choice2.innerHTML="";
+                
+                description.appendChild(descriptionText);
+                question.appendChild(questionText);
+                choice1.appendChild(choice1Text);
+                choice2.appendChild(choice2Text);
 
             }
         }
